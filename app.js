fdf4468145195a9807d4b4c9481c37b6e32003ed
Aliina08 <abdulhalikova.lina@yandex.ru>
2021-05-23 10:05:45 +0300

    Add tag (user interface)

--- app.js
+++ app.js
@@ -30,6 +30,25 @@ $(".tabs a span").toArray().forEach(function (element) {
 	$("main .content").append($content);
 	} 
 	else if ($element.parent().is(":nth-child(3)")) {
+		// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
+		console.log("Щелчок на вкладке Теги");
+		var organizedByTag = [
+			{ "name": "покупки","toDos": ["Купить продукты "]},
+			{ "name": "рутина","toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк "]},
+			/* и т. д. */
+		];
+		organizedByTag.forEach(function (tag) {
+			var $tagName = $("<h3>").text(tag.name),
+			$content = $("<ul>");
+			tag.toDos.forEach(function (description) {
+			var $li = $("<li>").text(description);
+			$content.append($li);
+			});
+			$("main .content").append($tagName);
+			$("main .content").append($content);
+			});
+	}
+	else if ($element.parent().is(":nth-child(4)")) {
 		$(".content").append(
 			'<input type="text" class="inp">'+
 			'<button class="btn">Добавить</button>'
